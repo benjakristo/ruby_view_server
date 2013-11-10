f45d7042712d858b44f5dd4ccb4188bb72cb210d
Benjakristo <benjamin@amasome.com>
2013-11-10 15:26:25 +0100

    Home for section 1 completed

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,7 @@
 require 'erb'
 
 x = 42
-template = ERB.new "The value of x is: <%= x %>"
+template = ERB.new("The value of x is: <%= x %>")
+template_two = ERB.new("The value of x is: <%= x %>")
+template_two = ERB.new("The value of x is: <%= x %>")
 puts template.result(binding)
